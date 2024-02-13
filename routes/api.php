902bb875a5937f098d18e435fbc888207c24727e
Leonardo Morais Franca <mfrancaleonardo@gmail.com>
2024-02-13 02:12:04 -0300

    feat: add routes
    Signed-off-by: Leonardo Morais Franca <mfrancaleonardo@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +19,30 @@ use Illuminate\Support\Facades\Route;
 // TODO - Verify how to avoid 'route [login] not defined' error
 
 Route::group([
-    'prefix' => 'auth'
+    'prefix' => 'auth',
 ], static function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
+});
+
+Route::group([
+    'prefix' => 'products',
+], static function () {
+    Route::get('/', [ProductsController::class, 'index']);
+    Route::get('/{id}', [ProductsController::class, 'show']);
+    Route::post('/', [ProductsController::class, 'store']);
+    Route::put('/{id}', [ProductsController::class, 'update']);
+    Route::delete('/{id}', [ProductsController::class, 'destroy']);
+});
+
+Route::group([
+    'prefix' => 'categories',
+], static function () {
+    Route::get('/', [CategoriesController::class, 'index']);
+    Route::get('/{id}', [CategoriesController::class, 'show']);
+    Route::post('/', [CategoriesController::class, 'store']);
+    Route::put('/{id}', [CategoriesController::class, 'update']);
+    Route::delete('/{id}', [CategoriesController::class, 'destroy']);
 });
